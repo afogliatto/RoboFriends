def63bc2b8ee4f78ec34b76dfa25d608aeda78a4
afogliatto <50391653+afogliatto@users.noreply.github.com>
2021-06-25 21:19:13 -0300

    Robo App Created

---
+++ src/ArrFun.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// const ArrFun = (props) => {
+//   return (
+//     <div className="f1 tc">
+//       <h2>This is the Arrow Function</h2>
+//       <p>{props.amI}</p>
+//     </div>
+//   );
+// };
+
+// export default ArrFun;
